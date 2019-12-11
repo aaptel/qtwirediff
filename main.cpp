5bbb31c7fbd8bf1fc3ec8c8ecb1438fe1d6f1173
Aurelien Aptel <aaptel@suse.com>
2019-12-11 21:21:46 +0100

    remove unused include

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
-#include "mainwindow.h"
 #include <QApplication>
-#include "trace.h"
-#include "diff.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
